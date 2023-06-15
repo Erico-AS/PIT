dd933f40713aa2585c28e014b823e6e79453d5b2
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2023-06-15 11:37:21 -0300

    Update Banco_Formap

--- service/Banco_Formap.sql
+++ service/Banco_Formap.sql
@@ -8,6 +8,18 @@ create table user
 	nome_user varchar(255) not null,
 	email varchar(255) not null,
 	senha varchar(255) not null,
+	About you varchar(200) not null,
+	Profile varchar(100) not null,
 	primary key(id)
 );
+
+create table publi 
+(
+	id int unsigned not null auto_increment,
+	titulo varchar(100) not null,
+	texto varchar(5000) not null,
+	fk_id_user int unsigned not null,
+	Foreing Key (fk_id_user) References user(id),
+	primary key(id)
+) ENGINE = innodb;
 
